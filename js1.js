7f2ad49dfa58728c3ab0140a415af67df55022b1
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-02-05 14:27:29 +0200

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,107 @@
+var number = 10;
+var string = "Hello, world!";
+var boolean = true;
+var array = [1, 2, 3];
+var object = { key: "value" };
+
+var a = 10; // Can be reassigned and redeclared
+let b = 20; // Can be reassigned but not redeclared
+const c = 30; // Cannot be reassigned or redeclared
+
+
+var number = 10; // Number
+var string = "Hello"; // String
+var boolean = true; // Boolean
+var nullValue = null; // Null
+var undefinedValue; // Undefined
+
+var array = [1, 2, 3]; // Array
+var object = { key: "value" }; // Object
+var functionObject = function() {}; // Function
+
+
+var x = 10; // x is a number
+x = "Hello"; // Now x is a string
+
+
+var x = "10";
+var y = "5";
+
+var sum = x + y; // Concatenates strings: "105"
+var difference = x - y; // Converts strings to numbers: 5
+
+
+var x = 10;
+var y = "Hello";
+
+console.log(typeof x); // "number"
+console.log(typeof y); // "string"
+
+
+
+var name = "Mike";
+var age = 29;
+
+var message = `My name is ${name} and I am ${age} years old.`;
+console.log(message); // "My name is Mike and I am 29 years old."
+
+
+var text = "Hello, world!";
+
+console.log(text.length); // 13
+console.log(text.toUpperCase()); // "HELLO, WORLD!"
+console.log(text.indexOf("world")); // 7
+console.log(text.substring(0, 5)); // "Hello"
+
+
+
+var fruits = ["Lemon", "Apricot", "Peach"];
+
+console.log(fruits.length); // 3
+console.log(fruits[0]); // "Lemon"
+console.log(fruits.indexOf("Apricot")); // 1
+fruits.push("Kiwi"); // Adds "Kiwi" to the end
+
+
+
+var person = {
+    name: "Mike",
+    age: 29,
+    isStudent: false
+};
+
+console.log(person.name); // "Mike"
+console.log(person.age); // 29
+console.log(person.isStudent); // false
+
+
+
+
+
+var sum = 10 + 5;
+var isGreaterThan = 10 > 5;
+var result = (10 * 5) / 2;
+
+
+
+
+
+var x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is less than or equal to 5");
+}
+
+
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("Mike");
+
+
+// Arrow function
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+
